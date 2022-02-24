747e233706e615851b7637086b8d192882acdbf9
devRe89 <develop.r89@gmail.com>
2022-02-24 20:43:30 -0300

    update Header

--- src/modules/shared/components/Header.js
+++ src/modules/shared/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <Link 
                 className="navbar-brand" 
                 to="/"
-            >Mi Sitio</Link>
+            >Mi Home</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
